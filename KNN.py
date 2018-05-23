5299e077245f3b5ea27b4914478b86186d44adae
bosoncode <suda1527403057@163.com>
2018-05-23 21:33:06 +0800

    import matplotlib

--- KNN.py
+++ KNN.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib as mp
 
 
 # load train data
