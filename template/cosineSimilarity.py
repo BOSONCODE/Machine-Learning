3022974d634dec6418fe5cc4dc753525b5390af6
BOSON <suda1527403057@163.com>
2018-05-24 11:28:22 +0800

    Update cosineSimilarity.py

--- template/cosineSimilarity.py
+++ template/cosineSimilarity.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-x = np.array([1, 1, 2, 1, 1, 1, 0, 0, 0], dtype = np.int)
-y = np.array([1, 1, 1, 0, 1, 1, 1, 1, 1], dtype = np.int)
+def CosineSimilarity(x, y):
+    x = np.array(x, dtype = np.int)
+    y = np.array(y, dtype = np.int)
+    return np.sum(x*y)/(np.linalg.norm(x)*np.linalg.norm(y))
 
-cos = np.sum(x*y)/(np.linalg.norm(x)*np.linalg.norm(y))
-print('余弦相似度: ', cos)
+if __name__ == '__main__':
+    x = np.array([1, 1, 2, 1, 1, 1, 0, 0, 0], dtype = np.int)   
+    y = np.array([1, 1, 1, 0, 1, 1, 1, 1, 1], dtype = np.int)
+    print(CosineSimilarity(x, y))
