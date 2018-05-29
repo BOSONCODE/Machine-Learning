4c697bbb3e4b71c9a381c07f6e429fe7ce9f80fd
bosoncode <suda1527403057@163.com>
2018-05-29 13:37:09 +0800

    submit the implementation of PCa

---
+++ Dimensionality-Reduction/PCA.py
@@ -0,0 +1,26 @@
+'''
+objective function: max_{P}\frac{1}{m-1}||P'(X - meanX)||^2
+=> min_{tr(P'AP)} s.t. P'P = I A = cov(X, meanX)
+'''
+import numpy as np
+
+#A: nxm n denotes the samplenum
+#P: m*d
+
+def PCA(X, d):
+    X = np.array(X, dtype = np.float)
+    meanX = np.mean(X, axis=0)
+    X = X - meanX
+
+    A = np.cov(X, rowvar = 0) #rowvar 表示每一行为一个样本
+    eigVals, eigVector = np.linalg.eig(np.mat(A))
+    eigValsIndice = np.argsort(eigVals)
+    d_eigValIndice = eigValsIndice[-1: - (d + 1):-1]
+    d_eigVector = eigVector[:, d_eigValIndice]
+    return d_eigVector
+
+if __name__ == '__main__':
+    a = [[1, 2, 3], [2, 3, 4]]
+    P = PCA(a, 2)
+    print(a@P)
+    
--- Tools/Image_Select.py
+++ Tools/Image_Select.py
@@ -7,28 +7,50 @@ import numpy as np
 
 #A:nxm, labels:1xn n denotes samplenum m denotes demension
 
-def Image_Select(A, labels, trainnumber):
-    N = len(A); M = len(A[0]);
-    data = np.empty((0, A.shape[1]))
+#rd_data: 样本数x维度 label: 样本数x1
+def Image_Select(rd_data, rd_labels, selected_images):
+    data = np.empty((0, rd_data.shape[1]))
     label = np.empty((0))
-    test_data = np.empty((0, A.shape[1]))
+    test_data = np.empty((0, rd_data.shape[1]))
     test_label = np.empty((0))
-    class_num = np.unique(labels)
-    for c in class_num:
-        c_index = np.where(labels == c)[0]
-        np.random.shuffle(c_index)
-        c_data = A[c_index[:trainnumber],:]
-        c_labels = labels[c_index[:trainnumber]]
-        data = np.concatenate((data, c_data), axis=0)
-        label = np.concatenate((label, c_labels), axis=0)
-        individual_test_data = A[c_index[trainnumber:],:]
-        individual_test_labels = labels[c_index[trainnumber:]]
+
+    unique_class = np.unique(rd_labels)
+    for individual in unique_class:
+        individual_idx = np.where(rd_labels == individual)[0]
+        np.random.shuffle(individual_idx)
+
+        individual_data = rd_data[individual_idx[:selected_images],:]
+        individual_labels = rd_labels[individual_idx[:selected_images]]
+
+        data = np.concatenate((data, individual_data), axis = 0)
+        label = np.concatenate((label, individual_labels), axis = 0)
+
+        individual_test_data = rd_data[individual_idx[selected_images:],:]
+        individual_test_labels = rd_labels[individual_idx[selected_images:]]
+
         test_data = np.concatenate((test_data, individual_test_data), axis = 0)
         test_label = np.concatenate((test_label, individual_test_labels), axis = 0)
+
+    # shuffle
+    idx = np.arange(data.shape[0])
+    np.random.shuffle(idx)
+    data = data[idx, :]
+    label = label[idx]
+
+    test_idx = np.arange(test_data.shape[0])
+    np.random.shuffle(test_idx)
+    test_idx = test_idx[:np.size(test_idx)]
+
+    test_data = test_data[test_idx, :]
+    test_label = test_label[test_idx]
+
+    #print("train size:", data.shape)
+    #print("test size:", test_data.shape)
+
     return data, label, test_data, test_label
 
 if __name__ == '__main__':
     A = np.array([[1, 2, 3, 8], [4, 5, 6, 9]]).transpose()
-    labels = np.array([1, 2, 1, 2]);
+    labels = np.array([1, 2, 1, 2])
     train_data, train_labels, test_data, test_labels = Image_Select(A, labels, 1)
     print(train_data, train_labels, test_data, test_labels)
